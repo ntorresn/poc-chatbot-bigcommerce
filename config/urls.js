709edff9cff0820712c8ea289f8a97dcce13f5fd
nTorres <nelson.torresperlaza@externos.bci.cl>
2024-10-23 07:05:14 -0500

    Cambio url

--- config/urls.js
+++ config/urls.js
@@ -1,6 +1,6 @@
 const graphURL = 'https://graph.facebook.com/v20.0/';
-// const pocURL = 'https://poc-api-e5mp.onrender.com/';
-const pocURL = 'http://127.0.0.1:4000/';
+const pocURL = 'https://poc-api-e5mp.onrender.com/';
+// const pocURL = 'http://127.0.0.1:4000/';
 const bigcommerceURL = 'https://api.bigcommerce.com/stores/mp2k4phx4c/v3/'
 const bigcommerceUrlImages = 'https://api.bigcommerce.com/stores/mp2k4phx4c/v3/catalog/products/'
 
